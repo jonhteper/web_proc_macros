47aa2a5e8cecfa7ffdbe03138704349a5660c5e3
jonhteper <jonhteper@protonmail.com>
2024-07-31 13:29:40 -0600

    add From trait impl for PartialStruct macro

--- src/partial_struct.rs
+++ src/partial_struct.rs
@@ -6,6 +6,7 @@ use quote::quote;
 pub fn partial_struct(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).expect("#[derive(PartialStruct)] failed to parse input");
     let (ident, traits) = get_name_traits(&ast);
+    let original_ident = ast.ident;
     let lifetimes: Vec<_> = ast.generics.lifetimes().collect();
     let type_params: Vec<_> = ast.generics.type_params().collect();
     let where_clause = &ast.generics.where_clause;
@@ -23,6 +24,7 @@ pub fn partial_struct(input: TokenStream) -> TokenStream {
     };
 
     let mut values_fields = Vec::new();
+    let mut fields_in_from = Vec::new();
     for field in fields {
         let mut skip = false;
         let mut other_attrs = Vec::new();
@@ -52,12 +54,24 @@ pub fn partial_struct(input: TokenStream) -> TokenStream {
             #(#[#other_attrs])*
             #field_vis #field_name: #field_type,
         });
+
+        fields_in_from.push(quote! {
+            #field_name: value.#field_name,
+        });
     }
 
     let expanded = quote! {
         #( #[#traits] )*
         pub struct #ident<#(#lifetimes,)* #(#type_params),*> #where_clause {
             #(#values_fields)*
+        }
+
+        impl<#(#lifetimes,)* #(#type_params),*> From<#original_ident> for #ident #where_clause {
+            fn from(value: #original_ident) -> #ident {
+                #ident {
+                    #(#fields_in_from)*
+                }
+            }
         }
     };
 
